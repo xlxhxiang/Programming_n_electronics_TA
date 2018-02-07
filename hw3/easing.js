2facac6312286f4fc704ce97c1b56de885c3a6dc
lei xiang <lxiang@cca.edu>
2018-02-07 08:55:00 -0800

    modified easing.js

--- hw3/easing.js
+++ hw3/easing.js
@@ -1,6 +1,11 @@
+	var rate = 0.1;
+	var x = 0;
+	var y = 0;
+
 function setup(){
 	
 	createCanvas(800,800);
+	
 
 }
 
@@ -8,15 +13,8 @@ function draw(){
 	
 	background(0);
 	
-	var rate = 0.9;
-	var x = 0;
-	var y = 0;
-	var targetX = mouseX;
-	var targetY = mouseY;
-
-	
-	x = targetX * rate + x * (1-rate);
-	y = targetY * rate + y * (1-rate);
+	x = mouseX * rate + x * (1-rate);
+	y = mouseY * rate + y * (1-rate);
 	
 	fill(0,255,0);
 	ellipse(x,y,50,50);
